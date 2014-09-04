d78c32b4e292e7eb1643078117f5a948d3b091a8
Chyi-Kwei Yau <chyikwei.yau@gmail.com>
2014-09-04 15:03:15 -0400

    change LDA example

--- LDA/lda_example.py
+++ LDA/lda_example.py
@@ -11,23 +11,22 @@ from lda import LDA
 
 
 def main():
-    n_samples = 2000
+    n_samples = 4000
     n_features = 1000
-    n_topics = 20
+    n_topics = 10
     n_top_words = 15
 
     dataset = fetch_20newsgroups(
         shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
 
-    vectorizer = CountVectorizer(token_pattern=r"(?u)\b[^\d\W]\w+\b",
-                                 max_df=0.9, max_features=n_features, min_df=2, stop_words='english')
+    vectorizer = CountVectorizer(max_df=0.8, max_features=n_features, min_df=3, stop_words='english')
 
     doc_word_count = vectorizer.fit_transform(dataset.data[:n_samples])
     lda = LDA(n_topics=n_topics, kappa=0.7,
               tau0=1024., n_jobs=4, random_state=0)
 
     feature_names = vectorizer.get_feature_names()
-    lda.fit(doc_word_count)
+    lda.fit(doc_word_count, max_iters=20, tol=1e-3)
     for topic_idx, topic in enumerate(lda.components_):
         print("Topic #%d:" % topic_idx)
         print(" ".join([feature_names[i]
