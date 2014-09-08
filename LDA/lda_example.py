cb97eebf6a9f5f3a9a3995f9f7aac7886fdc8e6f
Chyi-Kwei Yau <chyikwei.yau@gmail.com>
2014-09-08 18:13:34 -0400

    add partial_fit example

--- LDA/lda_example.py
+++ LDA/lda_example.py
@@ -10,7 +10,11 @@ from sklearn.feature_extraction.text import CountVectorizer
 from lda import onlineLDA
 
 
-def main():
+def lda_batch_example():
+    """
+    batch update
+    """
+
     n_samples = 4000
     n_features = 1000
     n_top_words = 15
@@ -18,7 +22,8 @@ def main():
     dataset = fetch_20newsgroups(
         shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
 
-    vectorizer = CountVectorizer(max_df=0.8, max_features=n_features, min_df=3, stop_words='english')
+    vectorizer = CountVectorizer(
+        max_df=0.8, max_features=n_features, min_df=3, stop_words='english')
 
     doc_word_count = vectorizer.fit_transform(dataset.data[:n_samples])
     lda = onlineLDA(kappa=0.7, tau=512., n_jobs=-1, random_state=0, verbose=1)
@@ -29,6 +34,43 @@ def main():
         print("Topic #%d:" % topic_idx)
         print(" ".join([feature_names[i]
                         for i in topic.argsort()[:-n_top_words - 1:-1]]))
+
+
+def lda_online_example():
+    """
+    split data into chunks, and run online update
+    """
+
+    def chunks(l, n):
+        for i in xrange(0, len(l), n):
+            yield l[i:i + n]
+
+    chunk_size = 2000
+    n_features = 1000
+    n_top_words = 15
+
+    dataset = fetch_20newsgroups(
+        shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
+    vectorizer = CountVectorizer(
+        max_df=0.8, max_features=n_features, min_df=3, stop_words='english')
+    lda = onlineLDA(
+        kappa=0.7, tau=512., n_jobs=-1, n_docs=1e4, random_state=0, verbose=1)
+
+    for chunk, doc_list in enumerate(chunks(dataset.data, chunk_size)):
+        if chunk == 0:
+            doc_mtx = vectorizer.fit_transform(doc_list)
+            feature_names = vectorizer.get_feature_names()
+        else:
+            doc_mtx = vectorizer.transform(doc_list)
+
+        # fit model
+        lda.partial_fit(doc_mtx)
+        print("\nTopics after training chunk %d:" % chunk)
+        for topic_idx, topic in enumerate(lda.components_):
+            print("Topic #%d:" % topic_idx)
+            print(" ".join([feature_names[i]
+                            for i in topic.argsort()[:-n_top_words - 1:-1]]))
+
 
 def lda_simple_example():
     # TODO: complete test
@@ -62,7 +104,7 @@ def lda_simple_example():
 
     doc_word_count = vectorizer.fit_transform(test_docs)
     lda = onlineLDA(n_topics=n_topics, kappa=0.7,
-              tau0=1024., random_state=0, n_jobs=-1)
+                    tau0=1024., random_state=0, n_jobs=-1)
     lda.fit(doc_word_count)
     feature_names = vectorizer.get_feature_names()
     for topic_idx, topic in enumerate(lda.components_):
@@ -71,4 +113,5 @@ def lda_simple_example():
                         for i in topic.argsort()[:-n_top_words - 1:-1]]))
 
 if __name__ == '__main__':
-    main()
+    # lda_batch_example()
+    lda_online_example()
